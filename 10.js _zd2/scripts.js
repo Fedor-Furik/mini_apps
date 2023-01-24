49b6bca56ae17bba42bca30133b65a16ff37271d
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2023-01-24 11:10:34 +0300

    Add files via upload

---
+++ 10.js _zd2/scripts.js
@@ -0,0 +1,7 @@
+let menu = document.getElementById('menu')
+document.getElementById('open_menu').addEventListener('click', () => {
+    menu.translateX(-120)
+})
+document.getElementById('close_menu').addEventListener('click', () => {
+    alert('hello')
+})
