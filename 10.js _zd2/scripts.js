9fc4b01d6a44741b3f774d073b2c2b6a2a7d15ac
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2023-01-29 15:52:43 +0300

    Delete 10.js _zd2 directory

--- 10.js _zd2/scripts.js
+++
@@ -1,7 +0,0 @@
-let menu = document.getElementById('menu')
-document.getElementById('open_menu').addEventListener('click', () => {
-    menu.translateX(-120)
-})
-document.getElementById('close_menu').addEventListener('click', () => {
-    alert('hello')
-})
