92c77089df980c3a78d853dccdcf85590eda43a6
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2023-01-29 15:52:59 +0300

    Add files via upload

--- 10.js _zd4/scripts.js
+++ 10.js _zd4/scripts.js
@@ -1,6 +1,18 @@
 
 document.body.querySelector('button').addEventListener('click', () => {
+    let div = document.createElement('div')
+    document.body.querySelector('main').appendChild(div)
     let p = document.createElement('p')
+    let but = document.createElement('button')
+    but.innerText = 'delete'
+    but.className = `del ${document.querySelector('input').value}`
+    p.className = `del ${document.querySelector('input').value}`
+    but.addEventListener('click', () => {
+        but.remove()
+        p.remove()
+    })
     p.innerText = document.querySelector('input').value
-    document.body.appendChild(p)
+    document.querySelector('input').value = ''
+    document.body.querySelector('div').appendChild(p)
+    document.body.querySelector('div').appendChild(but)
 })
