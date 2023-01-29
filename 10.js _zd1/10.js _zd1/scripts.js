8bebe929c1e8f98b5a4c560683ef4589668fd6df
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2023-01-29 14:38:45 +0300

    Delete 10.js _zd1 directory

--- 10.js _zd1/10.js _zd1/scripts.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('call-back').addEventListener('click',() => {
-    document.getElementById('sloi_2').style.visibility = 'visible';
-    document.getElementById('sloi_2').style.opacity = '100';
-})
-document.getElementById('cls').addEventListener('click',() => {
-    document.getElementById('sloi_2').style.opacity = '0';
-    document.getElementById('sloi_2').style.visibility = 'hidden';
-})
