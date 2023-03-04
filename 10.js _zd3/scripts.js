c8d5d501c3e58db636b48b370da2e4003028b83e
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2023-03-04 13:29:46 +0300

    Add files via upload

---
+++ 10.js _zd3/scripts.js
@@ -0,0 +1,10 @@
+
+document.getElementById('byrg').addEventListener('click', () => {
+    let pop = document.getElementById('pop')
+    if(pop.style.visibility === 'hidden'){
+        pop.style.visibility = 'visible'
+    } else {
+        pop.style.visibility = 'hidden'
+    }
+    
+})
