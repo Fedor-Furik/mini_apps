085f518e7bad732fa84cc51b1b8315ea4917ab96
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2023-03-26 15:22:06 +0300

    Delete 13.js _zd2 directory

--- 13.js _zd2/scripts.js
+++
@@ -1,9 +0,0 @@
-let httpRequest = new XMLHttpRequest();
-
-httpRequest.onload = function() {
-    let obj = JSON.parse(httpRequest.responseText);
-    console.log(obj.title);
-}
-
-httpRequest.open('GET', 'https://jsonplaceholder.typicode.com/todos');
-httpRequest.send();
