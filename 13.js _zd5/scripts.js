1adbefbda3119ee3e0c173865d641fe89f39bc4b
Fedor-Furik <107797365+Fedor-Furik@users.noreply.github.com>
2023-04-09 15:50:43 +0300

    Delete 13.js _zd5 directory

--- 13.js _zd5/scripts.js
+++
@@ -1,9 +0,0 @@
-let httpRequest = new XMLHttpRequest();
-
-httpRequest.onload = function() {
-    let obj = JSON.parse(httpRequest.responseText);
-    
-}
-
-httpRequest.open('GET', 'https://jsonplaceholder.typicode.com/comments');
-httpRequest.send();
